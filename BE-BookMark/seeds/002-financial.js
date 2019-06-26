a5fc56a9ebef80e43108f6879eb1d1c339513b79
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-06-25 22:52:46 -0700

    added Financial Seed

---
+++ BE-BookMark/seeds/002-financial.js
@@ -0,0 +1,34 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('financial').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('financial').insert([
+        { 
+          name: "Wells Fargo", 
+          site: "https://www.wellsfargo.com/" 
+        },
+        {
+          name: "Bank of America",
+          site: "https://www.bankofamerica.com/"
+        },
+        {
+          name: "Chase",
+          site: "https://secure01a.chase.com/web/auth/dashboard"
+        },
+        {
+          name: "Citi Bank",
+          site: "https://online.citi.com/US/login.do?locale=en_US"
+        },
+        {
+          name: "Capital One",
+          site: "https://www.capitalone.com/"
+        },
+        {
+          name: "American Express",
+          site: "https://www.americanexpress.com/"
+        }
+      ]);
+    });
+};
